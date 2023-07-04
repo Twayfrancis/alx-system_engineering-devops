31b364437513965e9a3cd677f7f141c3da4699fc
Twayfrancis <tawaifrancis@gmail.com>
2023-07-04 12:02:56 -0700

    fix requirement fail

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,4 +3,7 @@
 # The sender phone number or name (including country code if present)
 # The receiver phone number or name (including country code if present)
 # The flags that were used
-puts ARGV[0].scan(/\[from:(\+?\w*)\]\s\[to:(\+?\w*)\]\s\[flags:(\S*)\]/).join(',')
+from = ARGV[0].scan(/from:(.*?)\]/)
+to = ARGV[0].scan(/to:(.*?)\]/)
+flags = ARGV[0].scan(/flags:(.*?)\]/)
+puts [from, to, flags].join(',')
