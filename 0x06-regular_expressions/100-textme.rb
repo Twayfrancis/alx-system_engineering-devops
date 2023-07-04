63f7512b0b6815e7af9f5a04ae7b116c68e27276
Twayfrancis <tawaifrancis@gmail.com>
2023-07-04 11:56:10 -0700

    8. Textme

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# cript should output: [SENDER],[RECEIVER],[FLAGS]
+# The sender phone number or name (including country code if present)
+# The receiver phone number or name (including country code if present)
+# The flags that were used
+puts ARGV[0].scan(/\[from:(\+?\w+)\] \[to:(\+?\w+)\] \[flags:(-?\d:-?\d:-?\d:-?\d:-?\d)\]/).join(",")
