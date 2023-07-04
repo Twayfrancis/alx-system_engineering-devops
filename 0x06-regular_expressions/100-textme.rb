fc1e0d042dcaf108ee4bc06a477dee4aebfcbcc0
Twayfrancis <tawaifrancis@gmail.com>
2023-07-04 11:57:37 -0700

    8. Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,4 +3,4 @@
 # The sender phone number or name (including country code if present)
 # The receiver phone number or name (including country code if present)
 # The flags that were used
-puts ARGV[0].scan(/\[from:(\+?\w+)\] \[to:(\+?\w+)\] \[flags:(-?\d:-?\d:-?\d:-?\d:-?\d)\]/).join(",")
+puts ARGV[0].scan(/\[from:(\+?\w*)\]\s\[to:(\+?\w*)\]\s\[flags:(\S*)\]/).join(',')
