75195bd238a0852a741b810117dc8151dda1e019
Twayfrancis <tawaifrancis@gmail.com>
2023-10-11 00:55:00 +0300

    fix pycodestyle

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Recursive function that queries the Reddit API and returns
-	list containing the titles of all hot articles for a given subreddit. """
+    list containing the titles of all hot articles for a given subreddit. """
 
 import requests
 
@@ -8,9 +8,7 @@ import requests
 def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot articles on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     params = {
         "after": after,
         "count": count,
